043a8cc83603ed07ad327949f0e02e5b4c4f70da
Ewen Le Maux <149510405+EwenLM@users.noreply.github.com>
2026-06-12 09:12:10 +0200

    Delete library-backend/backend directory

--- library-backend/backend/src/main/java/com/library/factory/UsineAbstraite.java
+++
@@ -1,9 +0,0 @@
-package com.library.factory;
-
-import com.library.entities.Exemplaire;
-import com.library.entities.Ouvrage;
-
-public abstract class UsineAbstraite {
-    public abstract Ouvrage creerOuvrage(String titre, String genre, String auteurOuNumero);
-    public abstract Exemplaire creerExemplaire(Ouvrage ouvrage, String cote);
-}
