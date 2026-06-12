34e03fab94d2dd3b6c8b6bf871774269173946d9
EwenLM <ewenqglm@gmail.com>
2026-06-12 14:21:34 +0200

    chore(frontend): fix list livres

--- frontend/src/app/components/livre-list.component/livre-list.component.ts
+++ frontend/src/app/components/livre-list.component/livre-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LivreService } from '../../services/livre.service';
 import { Livre } from '../../models/livre.model';
@@ -10,13 +10,13 @@ import { Livre } from '../../models/livre.model';
   templateUrl: './livre-list.component.html',
 })
 export class LivreListComponent implements OnInit {
-  livres: Livre[] = [];
+  livres = signal<Livre[]>([]);
 
   constructor(private livreService: LivreService) {}
 
   ngOnInit(): void {
     this.livreService.getLivres().subscribe({
-      next: (data) => (this.livres = data),
+      next: (data) => this.livres.set(data),
       error: (err) => console.error('Erreur lors du chargement des livres', err),
     });
   }
